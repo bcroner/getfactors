112699e70363ea638c8f4c346470e579985a11fe
bcroner <seekandwander@gmail.com>
2025-05-24 14:36:44 -0700

    Update 3SAT_framework.hpp

--- 3SAT_framework.hpp
+++ 3SAT_framework.hpp
@@ -15,7 +15,7 @@ struct nlist { /* table entry: */
 
 typedef struct {
 
-    int id;
+    __int64 id;
 
 } bit_3sat;
 
@@ -24,9 +24,9 @@ typedef struct {
 typedef struct {
 
     bit_3sat** bits; // bit string
-    int   sz; // size in bits of number
-    int   bd_sz; // before decimal size
-    int   ad_sz; // after decimal size
+    __int64   sz; // size in bits of number
+    __int64   bd_sz; // before decimal size
+    __int64   ad_sz; // after decimal size
 
 } dec_3sat;
 
@@ -35,59 +35,59 @@ typedef struct {
 typedef struct {
 
     bit_3sat** bits; // bit string
-    int   sz; // size in bits of number
+    __int64   sz; // size in bits of number
 
 } nat_3sat;
 
-bit_3sat* create_bit(int* num_parm);
+bit_3sat* create_bit(__int64 * num_para);
 
-dec_3sat* create_dec(int* num_parm, int bd_sz_parm, int ad_sz_parm);
+dec_3sat* create_dec(__int64 * num_para, __int64 bd_sz_parm, __int64 ad_sz_parm);
 
-nat_3sat* create_nat(int* num_parm, int sz_parm);
+nat_3sat* create_nat(__int64 * num_para, __int64 sz_parm);
 
-char* and_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, bit_3sat* b, int * len_para);
+char* and_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* nand_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, bit_3sat* b, int* len_para);
+char* nand_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* or_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, bit_3sat* b, int* len_para);
+char* or_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* nor_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, bit_3sat* b, int* len_para);
+char* nor_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* not_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, int* len_para);
+char* not_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, __int64 * len_para);
 
-char* xor_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, bit_3sat* b, int* len_para);
+char* xor_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* xnor_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, bit_3sat* b, int* len_para);
+char* xnor_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* bitaddsum_3sat(int* num_parm, bit_3sat** sum, bit_3sat* c_in, bit_3sat* a, bit_3sat* b, int* len_para);
+char* bitaddsum_3sat(__int64 * num_para, bit_3sat** sum, bit_3sat* c_in, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* bitaddcout_3sat(int* num_parm, bit_3sat** c_out, bit_3sat* c_in, bit_3sat* a, bit_3sat* b, int* len_para);
+char* bitaddcout_3sat(__int64 * num_para, bit_3sat** c_out, bit_3sat* c_in, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* implies_3sat(int* num_parm, bit_3sat** c, bit_3sat* a, bit_3sat* b, int* len_para);
+char* implies_3sat(__int64 * num_para, bit_3sat** c, bit_3sat* a, bit_3sat* b, __int64 * len_para);
 
-char* dec_add(int* num_parm, dec_3sat** c, dec_3sat* a, dec_3sat* b, bool discard_final_c_out, __int64* len_para);
+char* dec_add(__int64 * num_para, dec_3sat** c, dec_3sat* a, dec_3sat* b, bool discard_final_c_out, __int64* len_para);
 
-char* nat_add(int* num_parm, nat_3sat** c, nat_3sat* a, nat_3sat* b, bool discard_final_c_out, __int64* len_para);
+char* nat_add(__int64 * num_para, nat_3sat** c, nat_3sat* a, nat_3sat* b, bool discard_final_c_out, __int64* len_para);
 
-char* twos_comp_3sat(int* num_parm, dec_3sat** c, dec_3sat* a, int* len_para);
+char* twos_comp_3sat(__int64 * num_para, dec_3sat** c, dec_3sat* a, __int64 * len_para);
 
-char* dec_sub(int* num_parm, dec_3sat** c, dec_3sat* a, dec_3sat* b, int* len_para);
+char* dec_sub(__int64 * num_para, dec_3sat** c, dec_3sat* a, dec_3sat* b, __int64 * len_para);
 
-char* dec_equals(int* num_parm, dec_3sat* a, dec_3sat* b, bool eq, __int64* len_para);
+char* dec_equals(__int64 * num_para, dec_3sat* a, dec_3sat* b, bool eq, __int64* len_para);
 
-char* nat_equals(int* num_parm, nat_3sat* a, nat_3sat* b, bool eq, __int64* len_para);
+char* nat_equals(__int64 * num_para, nat_3sat* a, nat_3sat* b, bool eq, __int64* len_para);
 
-char* dec_not_equals(int* num_parm, dec_3sat* a, dec_3sat* b, __int64* len_para);
+char* dec_not_equals(__int64 * num_para, dec_3sat* a, dec_3sat* b, __int64* len_para);
 
-char* nat_not_equals(int* num_parm, nat_3sat* a, nat_3sat* b, __int64* len_para);
+char* nat_not_equals(__int64 * num_para, nat_3sat* a, nat_3sat* b, __int64* len_para);
 
-char* dec_mul(int * num_parm, dec_3sat** c, dec_3sat* a, dec_3sat* b, int bd_sz, int ad_sz, __int64* len_para);
+char* dec_mul(__int64 * num_parm, dec_3sat** c, dec_3sat* a, dec_3sat* b, int bd_sz, int ad_sz, __int64* len_para);
 
-char* nat_mul(int* num_parm, nat_3sat** c, nat_3sat* a, nat_3sat* b, int sz, __int64* len_para);
+char* nat_mul(__int64 * num_para, nat_3sat** c, nat_3sat* a, nat_3sat* b, int sz, __int64* len_para);
 
-char* dec_div(int * num_parm, dec_3sat * c, dec_3sat * a, dec_3sat ** b, __int64* len_para);
+char* dec_div(__int64 * num_parm, dec_3sat * c, dec_3sat * a, dec_3sat ** b, __int64* len_para);
 
-char* dec_sqrt(int* num_parm, dec_3sat* c, dec_3sat** a, __int64* len_para);
+char* dec_sqrt(__int64 * num_para, dec_3sat* c, dec_3sat** a, __int64* len_para);
 
 //void bignum_from_str_oct(bn* c_bn, char* c_str, int len);
 
@@ -95,24 +95,24 @@ inline int octal_from_char(char c);
 
 //void bignum_from_str_oct(bn* c_bn, char* c_str, int len);
 
-inline int decimal_from_char(char c);
+inline __int64 decimal_from_char(char c);
 
-inline int int_from_hex_char(char c);
+inline __int64 int_from_hex_char(char c);
 
-inline char hex_char_from_int(int a);
+inline char hex_char_from_int(__int64 a);
 
-bool* hex2bool(const char* a, int sz);
+bool* hex2bool(const char* a, __int64 sz);
 
-//void bignum_from_str_dec(bn* c_bn, char* c_str, int len);
+//void bignum_from_str_dec(bn* c_bn, char* c_str, __int64 len);
 
-int** input_from_char_buf(char* buf_3sat, __int64 buf_3sat_sz, int* k, bool cnf);
+__int64** input_from_char_buf(char* buf_3sat, __int64 buf_3sat_sz, __int64* k, bool cnf);
 
-char* dec_to_str(bool* decodable_buf, dec_3sat* a, int* str_sz);
+char* dec_to_str(bool* decodable_buf, dec_3sat* a, __int64* str_sz);
 
-char* nat_to_str(bool* decodable_buf, nat_3sat* a, int* str_sz);
+char* nat_to_str(bool* decodable_buf, nat_3sat* a, __int64* str_sz);
 
-char* dec_get_factors(char* c_str, int c_str_buf_sz, int * len_para);
+char* dec_get_factors(char* c_str, __int64 c_str_buf_sz, __int64 * len_para);
 
-char* nat_get_factors(char* c_str, int c_str_buf_sz, int* len_para);
+char* nat_get_factors(char* c_str, __int64 c_str_buf_sz, __int64 * len_para);
 
 #endif // !__3SATFRAMEWORK_H__
