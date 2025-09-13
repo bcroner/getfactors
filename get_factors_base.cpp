8ee1c433b0b6272ef544cad62fffdd349a7fec8c
bcroner <seekandwander@gmail.com>
2025-09-13 12:11:31 -0700

    Update get_factors_base.cpp

--- get_factors_base.cpp
+++ get_factors_base.cpp
@@ -557,14 +557,14 @@ int main()
     //char* nat_mul_str = nat_test_mul(&len_para);
     //printf_s("%s\n", nat_mul_str);
 
-    char* equals_str = nat_test_equals(&len_para);
-    printf_s("%s\n", equals_str);
+    //char* equals_str = nat_test_equals(&len_para);
+    //printf_s("%s\n", equals_str);
 
-    /*
+    ///*
     char c_str[2000];
     //strcpy_s(c_str, 2000, "4");
-    strcpy_s(c_str, 2000, "5");
-    //strcpy_s(c_str, 2000, "6");
+    //strcpy_s(c_str, 2000, "5");
+    strcpy_s(c_str, 2000, "6");
     //strcpy_s(c_str, 2000, "7");
     //strcpy_s(c_str, 2000, "8");
     //strcpy_s(c_str, 2000, "F");
