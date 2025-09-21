96b19d9b4f8bff4ee9577b5be7a5ca426854d29f
bcroner <seekandwander@gmail.com>
2025-09-21 12:01:10 -0700

    Update 3SATBaseConsole.cpp

--- 3SATBaseConsole.cpp
+++ 3SATBaseConsole.cpp
@@ -223,7 +223,7 @@ bool SATSolver_add(SATSolver * me , __int64 cls_ix, __int64 prev) {
 
 		__int64 jump = 0;
 
-		if (me->master->powers[prev] < 0 && -me->master->powers[prev] == me->master->powers[pow]) {
+		if (/*me->master->powers[prev] < 0 && */-me->master->powers[prev] == me->master->powers[pow]) {
 
 
 			if (SATSolver_less_than(me->master->limits[prev], me->master->limits[pow])) {
@@ -290,7 +290,7 @@ __int64 SATSolver_initializePowJump(SATSolver* me, __int64 prev) {
 		__int64 prcsd_limit = prev < 0 ? 0 : SATSolver_less_than(me->master->limits[prev], temp_limit) ? me->master->limits[prev] : temp_limit;
 		if ((me->cls_tly[i] == 3 && abs_temp_jump > abs_max_jump) ||
 			(me->cls_tly[i] == 3 && abs_temp_jump == abs_max_jump && temp_limit_is_larger) ||
-			(me->cls_tly[i] == 3 && me->master->powers[prev] < 0 && - temp_jump == me->master->powers[prev] && SATSolver_less_than(max_jump, prcsd_limit)))
+			(me->cls_tly[i] == 3 && /*me->master->powers[prev] < 0 && */ -temp_jump == me->master->powers[prev] && SATSolver_less_than(max_jump, prcsd_limit)))
 		{
 			max_jump = temp_jump;
 			max_limit = temp_limit;
